96fd8e1fc988193a154f135bfa44bdabe7d7276a
chakyruukg <chakyruu.kg@gmail.com>
2023-09-14 16:32:15 +0600

    askattyky

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,28 +1,50 @@
-import React from 'react';
-import logo from "../../img/image 6.png"
+import React, { useEffect } from "react";
+import logo from "../../img/logo.png";
+import logotext from "../../img/logtext.png";
 import { AiOutlineGlobal } from "react-icons/ai";
 import { FaUser } from "react-icons/fa";
-import { NavLink } from 'react-router-dom';
-import "./index.scss"
+import { NavLink } from "react-router-dom";
+import "./index.scss";
+import 'aos/dist/aos.css'; // Импортируйте CSS стили AOS
+import AOS from 'aos';
 
 const Header = () => {
-    return (
-        <div>
-           <div id="header">
-            <div className="container">
-                <div className="header">
-                    <img src={logo} alt="" />
-                        <NavLink>Главная</NavLink>
-                        <NavLink>О нас</NavLink>
-                        <NavLink>Курсы</NavLink>
-                        <NavLink>Покупка/Продажа</NavLink>
-                    <button><FaUser/></button>
-                    <button><AiOutlineGlobal/></button>
-                </div>
+    useEffect(() => {
+        // Инициализация AOS
+        AOS.init();
+      }, []);
+  return (
+    <div>
+      <div id="header" >
+        <div className="container">
+          <div className="header">
+            <div
+              className="header--logo"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <img src={logo} alt="" style={{ width: "45%" }} data-aos="fade-down"/>
+              <img src={logotext} alt="" style={{ width: "75%" }} data-aos="fade-down"/>
             </div>
-           </div>
+            <NavLink data-aos="fade-down">Главная</NavLink>
+            <NavLink data-aos="fade-down">О нас</NavLink>
+            <NavLink data-aos="fade-down">Курсы</NavLink>
+            <NavLink data-aos="fade-down">Покупка/Продажа</NavLink>
+            <button data-aos="fade-down">
+              <AiOutlineGlobal />
+            </button>
+            <button data-aos="fade-down">
+              <FaUser />
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
+import React from "react";
+import Slider from "react-slick";
+import "./index.scss";
+import Produce from "../Produce";
+import { useEffect } from 'react';
+import 'aos/dist/aos.css'; // Импортируйте CSS стили AOS
+import AOS from 'aos';
 
 const Home = () => {
-    return (
-        <div>
-            <div id="home">
-                <div className="container">
-                    <div className="home">
-                        
-                    </div>
+    useEffect(() => {
+        // Инициализация AOS
+        AOS.init();
+      }, []);
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 5000,
+    cssEase: "ease-in-out",
+  };
+  return (
+    <>
+      <div>
+        <div id="home" data-aos="fade-right" data-aos-delay="600">
+          <div className="container">
+            <div className="home">
+              <Slider {...settings}>
+                <div className="home--slide">
+                  <div className="home--slide__text" data-aos="fade-right" data-aos-delay="500">
+                    <h1>Установлен новый мировой рекорд урожайности сои</h1>
+                  </div>
                 </div>
+                <div className="home--slide2">
+                  <div className="home--slide2__text">
+                    <h1>
+                      Агро.КГ - Надёжный партнер и помощник вашего Агро
+                      хозяйства
+                    </h1>
+                  </div>
+                </div>
+                <div className="home--slide">
+                  <div className="home--slide__text">
+                    <h1>Установлен новый мировой рекорд урожайности сои</h1>
+                  </div>
+                </div>
+                <div className="home--slide2">
+                  <div className="home--slide2__text">
+                    <h1>
+                      Агро.КГ - Надёжный партнер и помощник вашего Агро
+                      хозяйства
+                    </h1>
+                  </div>
+                </div>
+              </Slider>
             </div>
+          </div>
         </div>
-    );
+      </div>
+      <Produce/>
+    </>
+  );
 };
 
 export default Home;
